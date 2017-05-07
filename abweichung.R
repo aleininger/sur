f7b5999c3e82327ab7c93e3f5b1ebe200de1b200
tolpatsch <dix.jan@gmx.de>
2017-05-07 09:27:55 +0200

    correct years to grab tmp df

--- abweichung.R
+++ abweichung.R
@@ -13,15 +13,15 @@
 df$abweichung <- as.numeric(NA)
 
 for(i in 1:nrow(subset(df, jahr == 2017))) {
-tmp <- subset(df, jahr == 2017) %>% filter(datum < df$datum[i], datum > df$datum[i] - months(6))
-
-# Durchschnitt der anderen Institute
-andere <- tmp %>% filter(institut != df$institut[i], partei == df$partei[i]) %>%
-  summarise(stimmanteil = mean(stimmanteil, na.rm = T)) %>% as.numeric
-
-# Durchschnitt dieses Instituts
-institut <- tmp %>% filter(institut == df$institut[i], partei == df$partei[i]) %>%
-  summarise(stimmanteil = mean(stimmanteil, na.rm = T)) %>% as.numeric
-
-df$abweichung[i] <- round(institut - andere, 1)
+  tmp <- subset(df, jahr == 2017) %>% filter(datum < df$datum[i], datum > df$datum[i] - months(6))
+  
+  # Durchschnitt der anderen Institute
+  andere <- tmp %>% filter(institut != df$institut[i], partei == df$partei[i]) %>%
+    summarise(stimmanteil = mean(stimmanteil, na.rm = T)) %>% as.numeric
+  
+  # Durchschnitt dieses Instituts
+  institut <- tmp %>% filter(institut == df$institut[i], partei == df$partei[i]) %>%
+    summarise(stimmanteil = mean(stimmanteil, na.rm = T)) %>% as.numeric
+  
+  df$abweichung[i] <- round(institut - andere, 1)
 }
